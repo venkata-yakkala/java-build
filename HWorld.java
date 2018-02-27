5f562a62e451d049b36bc89ef11c728ab1e1ff8b
venkata-yakkala <venkata.yakkala@gmail.com>
2018-02-28 00:33:32 +0530

    Update HWorld.java

--- HWorld.java
+++ HWorld.java
@@ -1,6 +1,6 @@
 public class HWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World jenkins build trigger");
+      System.out.println("Hello, World jenkins build trigger poll scm");
    }
 }
