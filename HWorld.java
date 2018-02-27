a4913ad6e783e314a9f5eba7d0302f23a396ba51
venkata-yakkala <venkata.yakkala@gmail.com>
2018-02-28 00:39:16 +0530

    git hook trigger

--- HWorld.java
+++ HWorld.java
@@ -1,6 +1,6 @@
 public class HWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World jenkins auto build trigger poll scm");
+      System.out.println("Hello, World jenkins auto build trigger git hook");
    }
 }
